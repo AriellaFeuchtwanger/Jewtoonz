a63e2abf40d6061e39c18654fe15d03137a52108
lyarmark <lyarmark@student.touro.edu>
2016-11-29 00:58:24 -0500

    theme

--- JewtoonzASP/JewtoonzASP/Home.aspx
+++ JewtoonzASP/JewtoonzASP/Home.aspx
@@ -1,16 +1,2 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="JewtoonzASP.Home" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/JewtoonzMaster.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="JewtoonzASP.Home" %>
 
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
-    
-    </div>
-    </form>
-</body>
-</html>
--- JewtoonzASP/JewtoonzASP/JewtoonzMaster.Master.designer.cs
+++ JewtoonzASP/JewtoonzASP/JewtoonzMaster.Master.designer.cs
@@ -13,12 +13,12 @@ namespace JewtoonzASP {
     public partial class JewtoonzMaster {
         
         /// <summary>
-        /// form1 control.
+        /// placeholder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder placeholder;
     }
 }
