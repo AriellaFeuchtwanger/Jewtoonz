6a1a11fe98f46b1e8cbc4df11c5994af7c5c2b3d
AriellaFeuchtwanger <acfeuchtwanger@gmail.com>
2016-12-25 10:27:56 -0500

    Added Error Pages

---
+++ JewtoonzASP/JewtoonzASP/Errors/OtherErrors.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Whoops!" Language="C#" MasterPageFile="~/JewtoonzMaster.Master" AutoEventWireup="true" CodeBehind="OtherErrors.aspx.cs" Inherits="JewtoonzASP.Errors.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="placeholder" runat="server">
+    <h1>Whoops!</h1>
+
+    <h4>Looks like there was an error. Don't worry, we're working to fix it.</h4>
+</asp:Content>
--- JewtoonzASP/JewtoonzASP/Errors/OtherErrors.aspx.cs
+++ JewtoonzASP/JewtoonzASP/Errors/OtherErrors.aspx.cs
@@ -5,16 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace JewtoonzASP
+namespace JewtoonzASP.Errors
 {
-    public partial class JewtoonzMaster : System.Web.UI.MasterPage
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(Response.Cookies["Name"] != null)
-            {
-                //NameBox.Text = Response.Cookies["Name"]["Name"];
-            }
+            Response.TrySkipIisCustomErrors = true;
         }
     }
 }
